8c726a44486f92c1fe59f6d2486354e82d76101a
Uri Livshits <uri.livshits@gmail.com>
2021-05-13 13:31:00 +0300

    debugging knex heroku

--- server.js
+++ server.js
@@ -38,10 +38,10 @@ const db = knex({
     client: 'pg',
     connection: {
         connectionString: process.env.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-    },
+        // ssl: {
+        //   rejectUnauthorized: false
+        // }
+    }
 });
 
 //?defining app
